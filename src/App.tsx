587eae365f3af78ab1dfe745247bcef922085087
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-01-29 21:23:56 +0300

    Add: Passing Props to Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { FC, useState } from "react";
 
 import "./App.css";
 import InputFeild from "./components/InputFeild";
 
-const App = () => {
+const App: FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  console.log(todo);
   return (
     <div className="container">
-      <InputFeild />
+      <InputFeild todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { FC } from "react";
 import "./styles.css";
-const InputFeild = () => {
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputFeild = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
