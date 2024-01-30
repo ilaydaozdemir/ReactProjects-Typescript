8cb72b574e788dd43fdc43552bebcf30dc86b19d
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-01-30 19:41:51 +0300

    useRef hook with Typescript

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
 import "./styles.css";
 
 interface Props {
@@ -7,9 +7,17 @@ interface Props {
   handleAdd: (e: React.FormEvent) => void;
 }
 const InputFeild = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="input"
         placeholder="Enter a task"
         className="input__box"
